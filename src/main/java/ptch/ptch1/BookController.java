c64021e014385a8589c75afcb73d756b5e2b3a7e
Harshit Sharma <harshittech456@gmail.com>
2023-09-07 17:30:55 +0530

    Implemented Feature -1:List all Books

---
+++ src/main/java/ptch/ptch1/BookController.java
@@ -0,0 +1,24 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/books")
+public class BookController {
+
+    private final BookService bookService;
+
+    @Autowired
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping
+    public List<Book> listBooks() //method to List all Books(Feature-1)
+    {
+        return bookService.getAllBooks();
+    }
+}

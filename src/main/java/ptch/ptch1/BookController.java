8d579e2c7b4d62632743dc803642b1591bb6530e
Harshit Sharma <harshittech456@gmail.com>
2023-09-07 17:32:37 +0530

    Fixed indentation

--- src/main/java/ptch/ptch1/BookController.java
+++ src/main/java/ptch/ptch1/BookController.java
@@ -1,24 +1,21 @@
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("/v1/books")
 public class BookController {
+  private final BookService bookService;
 
-    private final BookService bookService;
+  @Autowired
+  public BookController(BookService bookService) {
+    this.bookService = bookService;
+  }
 
-    @Autowired
-    public BookController(BookService bookService) {
-        this.bookService = bookService;
-    }
-
-    @GetMapping
-    public List<Book> listBooks() //method to List all Books(Feature-1)
-    {
-        return bookService.getAllBooks();
-    }
+  @GetMapping
+  public List<Book> listBooks() { //method to List all Books(Feature-1)
+    return bookService.getAllBooks();
+  }
 }
